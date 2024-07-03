ca9d5f426d3cd38063a9633e1da8aff32cb03dfd
ffojna <ffojna36@gmail.com>
2024-07-03 18:55:25 +0200

    optymalizacja

--- main.py
+++ main.py
@@ -5,16 +5,50 @@ import scipy.fftpack
 import os, keyboard
 from PIL import Image, ImageTk
 import tkinter as tk
+import threading
 
 
 # paths to images (mapujesz tutaj odpowiednie obrazy (ścieżki) do dźwięków)
 NOTE_IMAGES = {
     "E2": "images/E2.png",
+    "F2": "images/F2.png",
+    "F#2": "images/F#2.png",
     "G2": "images/G2.png",
+    "G#2": "images/G#2.png",
     "A2": "images/A2.png",
+    "A#2": "images/A#2.png",
+    "B2": "images/B2.png",
+    "C3": "images/C3.png",
+    "C#3": "images/C#3.png",
     "D3": "images/D3.png",
-    "A4": "images/A4.png"
+    "D#3": "images/D#3.png",
+    "E3": "images/E3.png",
+    "F3": "images/F3.png",
+    "F#3": "images/F#3.png",
+    "G3": "images/G3.png",
+    "G#3": "images/G#3.png",
+    "A3": "images/A3.png",
+    "A#3": "images/A#3.png",
+    "B3": "images/B3.png",
+    "C4": "images/C4.png",
+    "C#4": "images/C#4.png",
+    "D4": "images/D4.png",
+    "D#4": "images/D#4.png",
+    "E4": "images/E4.png",
+    "F4": "images/F4.png",
+    "F#4": "images/F#4.png",
+    "G4": "images/G4.png",
+    "G#4": "images/G#4.png",
+    "A4": "images/A4.png",
+    "A#4": "images/A#4.png",
+    "B4": "images/B4.png",
+    "C5": "images/C5.png",
+    "C#5": "images/C#5.png",
+    "D5": "images/D5.png",
+    "D#5": "images/D#5.png",
+    "E5": "images/E5.png"
 }
+
 
 # general settings
 SAMPLE_FREQ = 44100                         # sample frequency in Hz
@@ -22,7 +56,7 @@ WINDOW_SIZE = 44100                         # window size of the DFT in samples
 WINDOW_STEP = 21050                         # step size of window
 WINDOW_T_LEN = WINDOW_SIZE / SAMPLE_FREQ    # length of window in seconds
 SAMPLE_T_LENGTH = 1 / SAMPLE_FREQ           # length between two samples in seconds (to też ze wzoru)
-windowSamples = [0 for _ in range(WINDOW_SIZE)]
+windowSamples = np.zeros(WINDOW_SIZE)
 closestNote = ''
 SAMPLE_DUR = 2      # sample length in seconds (tylko do testowania sounddevice)
 
@@ -47,20 +81,24 @@ def callback(indata, frames, time, status):
     if status:
         print(status)
     if any(indata):
-        windowSamples = np.concatenate((windowSamples, indata[:, 0]))
-        windowSamples = windowSamples[len(indata[:, 0]):]
+        windowSamples = np.roll(windowSamples, -frames)
+        windowSamples[-frames:] = indata[:, 0]
         magnitudeSpec = abs(scipy.fftpack.fft(windowSamples)[:len(windowSamples)//2])
         
-        for i in range(int(62/(SAMPLE_FREQ/WINDOW_SIZE))):
-            magnitudeSpec[i] = 0
+        magnitudeSpec[:int(62 / (SAMPLE_FREQ / WINDOW_SIZE))] = 0
         
         maxInd = np.argmax(magnitudeSpec)
         maxFreq = maxInd * (SAMPLE_FREQ / WINDOW_SIZE)
-        closestNote, closestPitch = find_closest_note(maxFreq)
+        new_closestNote, closestPitch = find_closest_note(maxFreq)
         
-        os.system('cls' if os.name=='nt' else 'clear')
-        diffPitch = maxFreq - closestPitch
-        print(f"Closest note: {closestNote} {diffPitch:.1f}")
+        if new_closestNote != closestNote:
+            closestNote = new_closestNote
+            os.system('cls' if os.name=='nt' else 'clear')
+            diffPitch = maxFreq - closestPitch
+            print(f"Closest note: {closestNote} {diffPitch:.1f}")
+            if closestNote in NOTE_IMAGES:
+                img_path = NOTE_IMAGES[closestNote]
+                update_image(img_path)
     else:
         print("no input")
 
@@ -70,6 +108,7 @@ def stop_loop():
     global running
     running = False
     print("Stopping...")
+    on_closing()
 keyboard.add_hotkey('esc', stop_loop)
 
 # main program
@@ -81,15 +120,27 @@ display = ImageTk.PhotoImage(image)
 label = tk.Label(root, image=display)
 label.pack()
 
-try:
-  with sd.InputStream(channels=1, callback=callback,
-    blocksize=WINDOW_STEP,
-    samplerate=SAMPLE_FREQ):
-    while running:
-        root.update_idletasks()
-        root.update()
-        if closestNote in NOTE_IMAGES:
-            img_path = NOTE_IMAGES[closestNote]
-            update_image(img_path)
-except Exception as e:
-    print(str(e))
+def start_audio_stream():
+    try:
+        with sd.InputStream(channels=1, callback=callback, blocksize=WINDOW_STEP, samplerate=SAMPLE_FREQ):
+            while running:
+                root.update_idletasks()
+                root.update()
+    except Exception as e:
+        print(str(e))
+
+audio_thread = threading.Thread(target=start_audio_stream)
+audio_thread.start()
+
+def on_closing():
+    global running
+    running = False
+    root.destroy()
+
+# closing window
+root.protocol("WM_DELETE_WINDOW", on_closing)
+root.mainloop()
+
+# ensure proper closing
+running = False
+audio_thread.join()
